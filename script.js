6364b3aaaf605937df65e0c6c0f3fb47fae0c119
oshilajawork-sketch <oshilajawork@gmail.com>
2026-06-19 19:06:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const menuToggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
+const navItems = document.querySelectorAll(".nav-links a");
 
+// Open/close menu
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+});
+
+// Close menu after clicking a link
+navItems.forEach(item => {
+    item.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
 });
